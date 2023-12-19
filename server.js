21294b6f52dbb7f6d75fe61db983094659c6d188
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-12-19 23:00:50 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const mapHandler = require('./api/map');
+const validateMapKeyHandler = require('./api/validate-map-key');
+const ipinfoHandler = require('./api/ipinfo');
+
+const app = express();
+const port = process.env.PORT || 8966;
+
+// 使用查询参数处理所有地图请求
+app.get('/api/map', mapHandler);
+
+// 使用查询参数处理所有 IP 地址请求
+app.get('/api/ipinfo', ipinfoHandler);
+
+// 设置静态文件服务
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.all('/api/validate-map-key', validateMapKeyHandler);
+
+// 启动服务器
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
